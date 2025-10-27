314dfb1c9e99e78d0b32e80496c32cbd94d3689f
Mark7625 <72366279+Mark7625@users.noreply.github.com>
2025-10-27 23:21:05 +0000

    Improved ContainerType
    - Improved Logging
    - Fixed Error when cant find a container

--- game-server/src/main/kotlin/org/alter/game/pluginnew/event/impl/ButtonClickEvent.kt
+++ game-server/src/main/kotlin/org/alter/game/pluginnew/event/impl/ButtonClickEvent.kt
@@ -1,20 +1,30 @@
 package org.alter.game.pluginnew.event.impl
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import net.rsprot.protocol.util.CombinedId
 import org.alter.game.model.entity.Player
 import org.alter.game.pluginnew.MenuOption
 import org.alter.game.pluginnew.event.PlayerEvent
+import org.alter.rscm.RSCM
 import org.alter.rscm.RSCM.asRSCM
 
 enum class ContainerType(val id: String) {
     INVENTORY("interfaces.inventory"),
     WORN_EQUIPMENT("interfaces.wornitems"),
     EQUIPMENT("interfaces.equipment"),
-
-    UNKNOWN("unknown");
+    SHOP("interfaces.shopmain");
 
     companion object {
-        fun fromId(id: Int): ContainerType = ContainerType.entries.find { it.id.asRSCM() == id } ?: UNKNOWN
+        val logger = KotlinLogging.logger {}
+
+        fun fromId(id: Int): ContainerType? {
+            val entry = entries.find { it.id.asRSCM() == id }
+            return entry ?: run {
+                val reverseName = RSCM.getReverseMapping("interfaces", id)
+                logger.info { "Missing mapping for id=$id (reverse: $reverseName) in ContainerType." }
+                null
+            }
+        }
     }
 }
 
@@ -30,7 +40,9 @@ data class ButtonClickEvent(
         if (item != -1) {
             val containerType = ContainerType.fromId(component.interfaceId)
             val option = MenuOption.fromId(option)
-            ItemClickEvent(item, slot, option, containerType, player).post()
+            if (containerType != null) {
+                ItemClickEvent(item, slot, option, containerType, player).post()
+            }
         }
     }
 }
